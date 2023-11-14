e422ea132aca6d22f12267c8d0cfccc1c6f34097
MasRangga <ranggah503@gmail.com>
2023-11-14 22:32:18 +0800

    UPDATE2

--- controllers/todoss-controller.js
+++ controllers/todoss-controller.js
@@ -3,10 +3,12 @@ const { todos } = require("../models");
 module.exports = {
   getAllTodo: async (req, res) => {
     try {
-      const todoList = await todos.findAll(); // Revisi: Ganti nama variabel agar lebih deskriptif
-      console.log(todoList);
+      const todoList = await todos.findAll();
 
-      res.status(200).json(todoList);
+      res.status(200).json({
+        message: "Berhasil mengambil semua todo",
+        data: todoList,
+      });
     } catch (error) {
       res.status(500).json({
         message: "Gagal mengambil data todo",
@@ -17,11 +19,14 @@ module.exports = {
 
   getTodoById: async (req, res) => {
     try {
-      const todoId = req.params.id; // Revisi: Ganti nama variabel agar lebih deskriptif
+      const todoId = req.params.id;
       const todo = await todos.findByPk(todoId);
 
       if (todo) {
-        res.status(200).json(todo);
+        res.status(200).json({
+          message: "Berhasil mengambil todo berdasarkan ID",
+          data: todo,
+        });
       } else {
         res.status(404).json({
           message: "Todo tidak ditemukan",
@@ -36,7 +41,7 @@ module.exports = {
   },
 
   createTodo: async (req, res) => {
-    const todoData = req.body; // Revisi: Ganti nama variabel agar lebih deskriptif
+    const todoData = req.body;
 
     try {
       await todos.create(todoData);
@@ -62,26 +67,36 @@ module.exports = {
         return res.status(404).json({ message: "Todo not found" });
       }
 
-      // Revisi: Menunggu pembaruan data selesai sebelum melanjutkan
       await todo.update({
         value: req.body.value,
         user_id: req.body.user_id,
       });
 
-      res.status(200).json({ message: "Todo berhasil diupdate" });
+      res.status(200).json({
+        message: "Todo berhasil diupdate",
+        data: todo,
+      });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: "Internal Server Error" });
+      res.status(500).json({
+        message: "Gagal mengupdate todo",
+        error: error.message,
+      });
     }
   },
 
   deleteAllTodo: async (req, res) => {
     try {
       await todos.destroy({ where: {} });
-      res.status(200).json({ message: "Semua todo berhasil dihapus" });
+      res.status(200).json({
+        message: "Semua todo berhasil dihapus",
+      });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: "Internal Server Error" });
+      res.status(500).json({
+        message: "Gagal menghapus semua todo",
+        error: error.message,
+      });
     }
   },
 
@@ -96,10 +111,15 @@ module.exports = {
       }
 
       await todo.destroy();
-      res.status(200).json({ message: "Todo berhasil dihapus" });
+      res.status(200).json({
+        message: "Todo berhasil dihapus",
+      });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: "Internal Server Error" });
+      res.status(500).json({
+        message: "Gagal menghapus todo",
+        error: error.message,
+      });
     }
   },
 };
